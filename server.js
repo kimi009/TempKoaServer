6ba0d727ea57a7589b5718f53495b25571fa3ee1
chenfuye <chenfuye@chinasofti.com>
2020-03-30 09:00:40 +0800

    test

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const koa = require("koa");
 // const Router = require("koa-router");
 const koaBodyParser = require("koa-bodyparser");
 const home = require("./home");
-const product = require('./product')
+const product = require("./product");
+const static = require("koa-static");
 const app = new koa();
+
+/*static(root,opts); root为静态文件资源下的文件目录,opts为可选项
+  opts: maxage浏览器缓存的最大寿命（以毫秒为单位）。默认为0
+        hidden允许传输隐藏文件。默认为false
+        index 默认文件名，默认为“ index.html”
+        defer如果为true，则在服务之后return next()，允许任何下游中间件首先响应。
+        gzip 当客户端支持gzip且所请求的扩展名为.gz的文件存在时，请尝试自动提供文件的gzip压缩版本。默认为true。
+        br 当客户端支持brotli并且存在所请求的扩展名为.br的文件时，请尝试自动提供文件的brotli版本（请注意，仅通过https接受brotli）。默认为true。
+        setHeaders函数，用于在响应时设置自定义标头。
+        extensionsURL中没有扩展名时，尝试匹配传递的数组中的扩展名以搜索文件。首次发现是送达的。（默认为false）
+*/
+app.use(static("./public"));
 // const router = new Router();
 app.use(
   koaBodyParser({
@@ -68,7 +81,7 @@ app.use(async (ctx, next) => {
 
 // app.use(router.routes()).use(router.allowedMethods());
 app.use(home.routes()).use(home.allowedMethods());
-app.use(product.routes()).use(product.allowedMethods())
+app.use(product.routes()).use(product.allowedMethods());
 
 app.listen(6010, "127.0.0.1", () => {
   console.log("server start...");
